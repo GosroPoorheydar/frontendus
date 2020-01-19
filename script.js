3a8b495ae987602f1e5cae81563d4e2c7eb68cb4
GosroPoorheydar <58260596+GosroPoorheydar@users.noreply.github.com>
2020-01-19 19:07:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function groot() {
 heen.addEventListener('click', groot);
 
 console.log(heen);
+
+
+/* met deze code vergroot de h1 in de header van de pagina door er op te klikken.
+Dit is een erg simpele interactie, maar ik gebruik zowel de queryselector als de 
+classlist om een dom element een class toe te wijzen.
+*/
